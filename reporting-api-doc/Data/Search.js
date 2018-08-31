d1f388e493d19b27b89b91042a1950186c242521
Yanren Long <yanren.long@hitachivantara.com>
2018-08-31 15:55:58 -0400

    Updated github page

---
+++ reporting-api-doc/Data/Search.js
@@ -0,0 +1,1 @@
+define({r:0.5,st:'Stem',n:1,t:[0],u:['../Content/ReportingAPI/reportingAPI-intro.htm'],s:['+pwz104r7erfs3vpnsi','sql'],p:[['+pwz104r7erfs3vpnsi',7],['change',7],['if',1],['ownedmodifications',3],['that',18]]});
---
+++ reporting-api-doc/Resources/Scripts/require.config.js
@@ -0,0 +1,3 @@
+require.config({
+    urlArgs: 't=636686485718650391'
+});
